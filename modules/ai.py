f95489d120664a49f54916ce22970cbfe6dfe877
Роман Семёнов <romansemenov2109@gmail.com>
2025-03-27 20:18:11 +0200

    change model

--- modules/ai.py
+++ modules/ai.py
@@ -16,7 +16,7 @@ client_openai = openai.AsyncOpenAI(api_key= OPENAI_SECRETKEY)
 async def get_responce_from_ai(request: str):
     # 
     response = await client_openai.chat.completions.create(
-        model="ft:gpt-4o-mini-2024-07-18:worldit::BFlQGLYC", #
+        model="ft:gpt-4o-mini-2024-07-18:worldit::BFm4aTNw", #
         messages = [{
             "role": "user", #
             "content": request, #
